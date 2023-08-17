8b93e43b40b6cb40cf3d2358169d1c10e975ed23
Ste <117104748+DaddySte@users.noreply.github.com>
2023-08-17 16:42:28 +0200

    Update Loader.cpp

--- NametagESP/Loader.cpp
+++ NametagESP/Loader.cpp
@@ -10,7 +10,7 @@ int main() {
 	while (true) {
 		if (GetAsyncKeyState('D') & 0x8000) {
 			is_enabled = !is_enabled;
-			while (GetAsyncKeyState('D') & 0x8000) std::this_thread::sleep_for(std::chrono::milliseconds(1)); //Used to avoid to run multiple safewalk threads
+			while (GetAsyncKeyState('D') & 0x8000) std::this_thread::sleep_for(std::chrono::milliseconds(1)); //Used to avoid to run multiple esp threads
 			enableESP(is_enabled);
 		}
 		else std::this_thread::sleep_for(std::chrono::milliseconds(1));
